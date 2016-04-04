4c255254f205a67874a7594fdd1f471a64f7f7da
kardoj <kardoj@users.noreply.github.com>
2016-04-04 20:23:44 +0300

    added topic routes and config file

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 var express = require('express');
-var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var config = require('./config');
 
-app.get('/api', function(req, res){
-  console.log('Somebody visited /api');
-  res.json({"success": "Tere tulemast!"});
+var app = express();
+app.use(morgan('tiny'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes are matched from top to bottom, 404 should be the bottom one!
+var topics = require('./routes/topics');
+app.use('/api/topics', topics);
+
+app.use(function(req, res, next) {
+    var err = new Error('Resource not found');
+    err.status = 404;
+    next(err); // Moves execution to the next error handler
+});
+
+app.use(function(err, req, res, next) {
+    console.error("["+(err.status || 500)+"] "+(new Date()).toString()+" "+req.url +' '+ err);
+    var message = err.status == 404 ? err.message : "Unknown error";
+    res.status(err.status || 500).json({
+        status: err.status || 500,
+        message: message //should by default hide in production
+    });
+});
+
+process.on('uncaughtException', function (err) {
+    console.error((new Date()).toString() + ' uncaughtException:', err.message);
+    console.error(err.stack);
+    process.exit(1);
 });
 
 app.listen(3000, function(){
